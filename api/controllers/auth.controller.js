d74d7e9deebf47b39df8082fb823180c16336e3a
ashwani30121999 <ashmsdbigfan@gmail.com>
2024-02-17 15:25:40 +0530

    vv

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -6,7 +6,7 @@ export const signup = async (req, res, next) => {
   const newUser = new User({ username, email, password: hashedPassword })
   try {
     await newUser.save()
-    res.status(201).json("Uer created successfully!")
+    res.status(201).json("Uer crejated successfully!")
   } catch (error) {
     next(error)
   }
